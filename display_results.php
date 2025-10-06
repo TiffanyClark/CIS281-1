444517694ea989824259f55c910a9d9c2cc91941
Tiffany <35204671+TiffanyClark@users.noreply.github.com>
2025-10-05 19:57:17 -0500

    chapter 6 files
    Assignment 6-1

--- display_results.php
+++ display_results.php
@@ -1,30 +1,30 @@
 <?php
-    // get the data from the form
-    $investment = filter_input(INPUT_POST, 'investment',
-        FILTER_VALIDATE_FLOAT);
-    $interest_rate = filter_input(INPUT_POST, 'interest_rate',
-        FILTER_VALIDATE_FLOAT);
-    $years = filter_input(INPUT_POST, 'years',
-        FILTER_VALIDATE_INT);
 
-    // set default error message of empty string
+    $investment = filter_input(INPUT_POST, 'investment', 
+            FILTER_VALIDATE_FLOAT);
+    $interest_rate = filter_input(INPUT_POST, 'interest_rate', 
+            FILTER_VALIDATE_FLOAT);
+    $years = filter_input(INPUT_POST, 'years', 
+            FILTER_VALIDATE_INT);
+
+
     $error_message = '';
     
-    // validate investment
+
     if ($investment === FALSE ) {
         $error_message .= 'Investment must be a valid number.<br>'; 
     } else if ( $investment <= 0 ) {
         $error_message .= 'Investment must be greater than zero.<br>'; 
     } 
     
-    // validate interest rate
+
     if ( $interest_rate === FALSE )  {
         $error_message .= 'Interest rate must be a valid number.<br>'; 
-    } else if ( $interest_rate <= 15 ) {
-        $error_message .= 'Interest rate must be less than or equal to 15.<br>';
+    } else if ( $interest_rate <= 0 ) {
+        $error_message .= 'Interest rate must be greater than zero.<br>'; 
     } 
     
-    // validate years
+
     if ( $years === FALSE ) {
         $error_message .= 'Years must be a valid whole number.<br>';
     } else if ( $years <= 0 ) {
@@ -33,18 +33,19 @@
         $error_message .= 'Years must be less than 31.<br>';
     } 
 
-    // if an error message exists, go to the index page
+
     if ($error_message != '') {
         include('index.php');
-        exit(); }
-
-    // calculate the future value
-    $future_value = $investment;
-    for ($i = 1; $i <= $years; $i++) {
-        $future_value += $future_value * $interest_rate * .01; 
+        exit();
     }
 
-    // apply currency and percent formatting
+
+    $future_value = $investment;
+    for ($i = 1; $i <= $years; $i++) {
+        $future_value += $future_value * $interest_rate; 
+    }
+
+
     $investment_f = '$'.number_format($investment, 2);
     $yearly_rate_f = $interest_rate.'%';
     $future_value_f = '$'.number_format($future_value, 2);
@@ -53,7 +54,7 @@
 <html>
 <head>
     <title>Future Value Calculator</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="main.css"/>
 </head>
 <body>
     <main>
--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php 
-    //set default value of variables for initial page load
+
     if (!isset($investment)) { $investment = ''; } 
     if (!isset($interest_rate)) { $interest_rate = ''; } 
     if (!isset($years)) { $years = ''; } 
-?> 
-<!DOCTYPE html>
+?>
+<!DOCTYPE html">
 <html>
 <head>
     <title>Future Value Calculator</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="main.css"/>
 </head>
 
 <body>
@@ -22,18 +22,17 @@
         <div id="data">
             <label>Investment Amount:</label>
             <input type="text" name="investment"
-                   value="<?php echo htmlspecialchars($investment); ?>">
+                   value="<?php echo $investment; ?>">
             <br>
 
             <label>Yearly Interest Rate:</label>
             <input type="text" name="interest_rate"
-                   value="<?php echo htmlspecialchars($interest_rate); ?>">
+                   value="<?php echo $interest_rate; ?>">
             <br>
 
             <label>Number of Years:</label>
             <input type="text" name="years"
-                   value="<?php echo htmlspecialchars($years); ?>">
-            <br>
+                   value="<?php $years; ?>"><br>
         </div>
 
         <div id="buttons">
